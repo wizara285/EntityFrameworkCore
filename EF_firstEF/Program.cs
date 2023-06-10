ef9ce24cebb47481793b5e16bf4bfe53e1facd1f
Bartosh_Andrew <bartosha285@gmail.com>
2023-06-10 14:20:37 +0300

    just second commit...

--- EF_firstEF/Program.cs
+++ EF_firstEF/Program.cs
@@ -9,30 +9,30 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //using (ApplicationContext context = new ApplicationContext())
-        //{
-        //    Position manager = new Position { Name = "Manager" };
-        //    Position developer = new Position { Name = "Developer" };
-        //    context.Positions.AddRange(manager, developer);
+        using (ApplicationContext context = new ApplicationContext())
+        {
+            Position manager = new Position { Name = "Manager" };
+            Position developer = new Position { Name = "Developer" };
+            context.Positions.AddRange(manager, developer);
 
-        //    City washington = new City { Name = "Washington" };
-        //    context.Cities.Add(washington);
+            City washington = new City { Name = "Washington" };
+            context.Cities.Add(washington);
 
-        //    Country usa = new Country { Name = "USA", Capital = washington };
-        //    context.Countries.Add(usa);
+            Country usa = new Country { Name = "USA", Capital = washington };
+            context.Countries.Add(usa);
 
-        //    Company microsoft = new Company { Name = "Microsoft", Country = usa };
-        //    Company google = new Company { Name = "Google", Country = usa };
-        //    context.Companies.AddRange(microsoft, google);
+            Company microsoft = new Company { Name = "Microsoft", Country = usa };
+            Company google = new Company { Name = "Google", Country = usa };
+            context.Companies.AddRange(microsoft, google);
 
-        //    Employee tom = new Employee { Name = "Tom", Company = microsoft, Position = manager };
-        //    Employee bob = new Employee { Name = "Bob", Company = google, Position = developer };
-        //    Employee alice = new Employee { Name = "Alice", Company = microsoft, Position = developer };
-        //    Employee kate = new Employee { Name = "Kate", Company = google, Position = manager };
-        //    context.Employees.AddRange(tom, bob, alice, kate);
+            Employee tom = new Employee { Name = "Tom", Company = microsoft, Position = manager };
+            Employee bob = new Employee { Name = "Bob", Company = google, Position = developer };
+            Employee alice = new Employee { Name = "Alice", Company = microsoft, Position = developer };
+            Employee kate = new Employee { Name = "Kate", Company = google, Position = manager };
+            context.Employees.AddRange(tom, bob, alice, kate);
 
-        //    context.SaveChanges();
-        //}
+            context.SaveChanges();
+        }
 
         using (ApplicationContext context = new ApplicationContext())
         {
@@ -43,5 +43,7 @@ public class Program
                 Console.WriteLine($"{employee.Name} - {employee.Position.Name} from {employee.Company.Name}");
             }
         }
+
+        //add
     }
 }
